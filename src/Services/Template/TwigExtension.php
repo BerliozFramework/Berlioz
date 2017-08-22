e7d532b54feb05d0be3e72186120bd8760374ee4
Ronan Giron <ElGigi@users.noreply.github.com>
2017-08-22 16:45:14 +0200

    Add 'json_decode' filter and 'instance of' test to the Twig extension

--- src/Services/Template/TwigExtension.php
+++ src/Services/Template/TwigExtension.php
@@ -64,6 +64,7 @@ class TwigExtension extends \Twig_Extension
         $filters[] = new \Twig_Filter('truncate', 'b_truncate');
         $filters[] = new \Twig_Filter('nl2p', 'b_nl2p', ['is_safe' => ['html']]);
         $filters[] = new \Twig_Filter('human_file_size', 'b_human_file_size');
+        $filters[] = new \Twig_Filter('json_decode', 'json_decode');
 
         return $filters;
     }
@@ -71,9 +72,9 @@ class TwigExtension extends \Twig_Extension
     /**
      * Filter to format date
      *
-     * @param  \DateTime|int $datetime DateTime object or timestamp
-     * @param  string        $pattern  Pattern of date result waiting
-     * @param  string        $locale   Locale for pattern translation
+     * @param \DateTime|int $datetime DateTime object or timestamp
+     * @param string        $pattern  Pattern of date result waiting
+     * @param string        $locale   Locale for pattern translation
      *
      * @return string
      */
@@ -84,6 +85,33 @@ class TwigExtension extends \Twig_Extension
         }
 
         return b_date_format($datetime, $pattern, $locale);
+    }
+
+    /**
+     * Returns a list of tests to add to the existing list.
+     *
+     * @return \Twig_Filter[]
+     */
+    public function getTests()
+    {
+        $tests = [];
+
+        $tests[] = new \Twig_Test('instance of', [$this, 'testInstanceOf']);
+
+        return $tests;
+    }
+
+    /**
+     * Test instance of.
+     *
+     * @param mixed  $object     The tested object
+     * @param string $class_name The class name
+     *
+     * @return bool
+     */
+    public function testInstanceOf($object, $class_name): bool
+    {
+        return is_a($object, $class_name, true);
     }
 
     /**
