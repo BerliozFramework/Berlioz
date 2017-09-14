c6c5605d43a4238c7b72b3022f73a4609a0b280a
Ronan Giron <ElGigi@users.noreply.github.com>
2017-09-14 10:04:22 +0200

    Add b_minify_html() global function

--- src/functions.php
+++ src/functions.php
@@ -514,6 +514,52 @@ function b_remove_entities(string $str): string
 }
 
 /**
+ * Minify HTML string.
+ *
+ * @param string $str
+ *
+ * @return string
+ * @link https://stackoverflow.com/a/5324014
+ */
+function b_minify_html(string $str): string
+{
+    // Save and change PHP configuration value
+    $oldPcreRecursionLimit = ini_get('pcre.recursion_limit');
+    if (PHP_OS == 'WIN') {
+        ini_set('pcre.recursion_limit', '524');
+    } else {
+        ini_set('pcre.recursion_limit', '16777');
+    }
+
+    $regex = <<<EOT
+%# Collapse whitespace everywhere but in blacklisted elements.
+(?>             # Match all whitespans other than single space.
+  [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+| \s{2,}        # or two or more consecutive-any-whitespace.
+) # Note: The remaining regex consumes no text at all...
+(?=             # Ensure we are not in a blacklist tag.
+  [^<]*+        # Either zero or more non-"<" {normal*}
+  (?:           # Begin {(special normal*)*} construct
+    <           # or a < starting a non-blacklist tag.
+    (?!/?(?:textarea|pre|script)\b)
+    [^<]*+      # more non-"<" {normal*}
+  )*+           # Finish "unrolling-the-loop"
+  (?:           # Begin alternation group.
+    <           # Either a blacklist start tag.
+    (?>textarea|pre|script)\b
+  | \z          # or end of file.
+  )             # End alternation group.
+)  # If we made it here, we are not in a blacklist tag.
+%Six
+EOT;
+
+    // Reset PHP configuration value
+    ini_set('pcre.recursion_limit', $oldPcreRecursionLimit);
+
+    return preg_replace($regex, ' ', $str);
+}
+
+/**
  * Treat string for url
  *
  * @param string $str
