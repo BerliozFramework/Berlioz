8bde9d209887fbb76f1ca45634ec371fc2cadd4f
Ronan Giron <ElGigi@users.noreply.github.com>
2018-08-06 12:35:40 +0200

    Fix router for Nginx

--- src/Services/Routing/Router.php
+++ src/Services/Routing/Router.php
@@ -216,6 +216,10 @@ class Router implements RouterInterface
             foreach ($_SERVER as $name => $value) {
                 if (substr($name, 0, 5) == 'HTTP_') {
                     $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                } else {
+                    if ($name === 'CONTENT_TYPE') {
+                        $headers['Content-Type'] = $value;
+                    }
                 }
             }
         }
