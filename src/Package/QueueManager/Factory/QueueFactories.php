69db06aa63a18314346bd2e46ed60e5804fe59c0
Ronan Giron <ElGigi@users.noreply.github.com>
2026-02-18 12:31:52 +0100

    Code-style

--- src/Package/QueueManager/Factory/QueueFactories.php
+++ src/Package/QueueManager/Factory/QueueFactories.php
@@ -44,7 +44,7 @@ class QueueFactories
      */
     public function createFromConfig(array $config): iterable
     {
-            $config['type'] ?? throw new ConfigException(sprintf('No queue type given'));
+        $config['type'] ?? throw new ConfigException(sprintf('No queue type given'));
 
         if (false === array_key_exists($config['type'], $this->factories)) {
             throw new ConfigException(sprintf('No factory found for queue of type "%s"', $config['type']));
